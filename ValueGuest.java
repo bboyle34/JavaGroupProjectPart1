cfacac262de36af29dd378b0ec86a8790eaea1a8
Brendan Boyle <bboyle34@gmail.com>
2019-11-19 20:25:51 -0500

    Update ValueGuest.java
    changed for arrayLists

--- ValueGuest.java
+++ ValueGuest.java
@@ -8,15 +8,11 @@ public class ValueGuest extends Guest
     private String savingsNumber;
     private int numberOfBookings;
     private double amountSpentWithHotel;
-    
-    public static ValueGuest[] valueGuests = new ValueGuest[1000];
-    public static int nextValueGuest = 0;
 
     // Constructors
     public ValueGuest(String username, String password, String guestName)
     {
         super(username, password, guestName);
-        valueGuests[nextValueGuest++] = this;
     }
 
     // Methods
@@ -31,14 +27,5 @@ public class ValueGuest extends Guest
     public String getSavingsNumber()
     {
         return this.savingsNumber;
-    }
-    public static int nextSpot()
-    {
-        int position = 0;
-        while (valueGuests[position + 1] != null)
-        {
-            position++;
-        }
-        return position;
     }
 }
