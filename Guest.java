d4d12056c61f76373fd209a9e4e32e932b89bde2
taylo8am <57958597+taylo8am@users.noreply.github.com>
2019-11-24 18:25:36 -0500

    Update Guest.java
    added comments

--- Guest.java
+++ Guest.java
@@ -1,4 +1,6 @@
-
+//Description: A class that defines specific guest objects for the hotel. Contains methods
+//to describe guests and also a method to check login credentials
+//contributed by Brendan Boyle
 package GroupProject1;
 // Class Definition File (CDF)
 
@@ -22,6 +24,8 @@ public class Guest
     }
 
     // Methods
+    //checks to ensure that credentials sent in as username and password
+    //match ones stored in memory upon creation of guest object.
     public boolean checkCredentials(String username, String password)
     {
         if (this.username.equals(username) && this.password.equals(password))
@@ -33,18 +37,27 @@ public class Guest
             return false;
         }
     }
+    
+    //getter for guest name
     public String getGuestName()
     {
         return this.guestName;
     }
+    
+    //getter for username
     public String getUsername()
     {
         return this.username;
     }
+    
+    //setter for guest name
     public void setGuestName(String guestName)
     {
         this.guestName = guestName;
     }
+    
+    //setter for guest password, requires old and new
+    //password as parameters
     public int setPassword(String oldP, String newP)
     {
         if (this.password.equalsIgnoreCase(oldP))
@@ -53,6 +66,8 @@ public class Guest
         }
         return 0;
     }
+    
+    //method which returns as a string a description of a guest
     public String describeGuest()
     {
         String answer = "";
