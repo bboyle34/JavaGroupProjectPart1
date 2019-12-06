0a43770a187ca54d2d8a0ba14a6d88c187c11605
GeorgeMoyaDev <georgemoya09@gmail.com>
2019-12-06 11:29:34 -0500

    Update Guest.java
    I commented out the setPassword method that uses the regex methods.
    And I implemented my own setPassword method that returns an int. And it checks for the password requirements needed.
    The FX application should display the correct label when any there is a missing password requirements

--- Guest.java
+++ Guest.java
@@ -60,75 +60,82 @@ public class Guest
     
     //setter for guest password, requires old and new
     //password as parameters
-   public int setPassword(String oldP, String newP)
-    {
-        boolean operator = true;
-        if (newP.substring(0, 1).matches("[0-9]"))
-        {
-            operator = false;
-        }
-        int count = 1;
-        b = false;
-        for (int i = 0; i < newP.length(); i++)
-        {
-            if (newP.substring(i).matches("[^A-Za-z0-9]"))
-            {
-                b = true;
-            }
-            count++;
-        }
-        Pattern p = Pattern.compile("[^A-Za-z0-9]");
-        Matcher m = p.matcher(newP);
-        boolean b = m.find();
-        if (this.password.equalsIgnoreCase(oldP) && b && operator)
-        {
-            System.out.println("it worked");
-            this.password = newP;
-            return 0;
-        }
-        
-        else
-        {
-            return 1;
-        }
-    }
-    
-    
-//        public boolean setPassword(String oldP, String newP)
+//   public int setPassword(String oldP, String newP)
 //    {
-//     
-//        boolean meetsPassRequirements = true;
-//        
-//        if(Character.isDigit(newP.charAt(0)))
+//        boolean operator = true;
+//        if (newP.substring(0, 1).matches("[0-9]"))
 //        {
-//            meetsPassRequirements = false;
+//            operator = false;
 //        }
-//        
-//        
-//           if(oldP.
-//                meetsPassRequirements = false;
-//        
-//        
-//        for(int i = 0; i < newP.length(); i++)
+//        int count = 1;
+//        boolean b = false;
+//        for (int i = 0; i < newP.length(); i++)
 //        {
-//            if(Character.isLowerCase(newP.charAt(i)))
+//            if (newP.substring(i).matches("[^A-Za-z0-9]"))
 //            {
-//                meetsPassRequirements = false;
+//                b = true;
 //            }
-//            
+//            count++;
+//        }
+//        Pattern p = Pattern.compile("[^A-Za-z0-9]");
+//        Matcher m = p.matcher(newP);
+//        b = m.find();
+//        if (this.password.equalsIgnoreCase(oldP) && b && operator)
+//        {
+//            System.out.println("it worked");
+//            this.password = newP;
+//            return 0;
 //        }
 //        
-//        for(int i = 0; i < newP.length(); i++)
+//        else
 //        {
-//            if(Character.isLetter(newP.charAt(i)))
-//            {
-//                meetsPassRequirements = false;
-//            }
+//            return 1;
 //        }
-//        return meetsPassRequirements;     
 //    }
 //    
     
+        public int setPassword(String oldP, String newP)
+    {
+     
+        int count = 0;
+        
+        if(Character.isDigit(newP.charAt(0))||oldP.matches(newP))
+        {
+            count = 1; 
+            return count;
+        }
+             
+       for(int i = 0; i < newP.length(); i++)
+        {
+            if(Character.isUpperCase(newP.charAt(i)))
+            {
+                count = 0;
+                break;
+            } 
+            else{
+                count = 1;
+                return count;
+                
+            }     
+        }
+        
+        
+        for(int i = 1; i < newP.length(); i++)
+        {
+            if(Character.isDigit(newP.charAt(i)))
+            {
+                count = 0;
+                return count;
+            }
+            else{
+                count = 1;
+               // return count;
+            }
+            
+        }
+        return count;
+    }
+            
     //method which returns as a string a description of a guest
     public String describeGuest()
     {
