02dc82d10ee81f3c5025d7cec73f7ff987fde6de
Brendan Boyle <bboyle34@gmail.com>
2019-12-07 16:48:46 -0500

    Add files via upload
    added WIP ddl

---
+++ HotelMadisonDDL.sql
@@ -0,0 +1,47 @@
+CREATE TABLE ROOM (
+	roomID Numeric(3) primary key, 
+	roomNumber Numeric(4),
+	bedOption Numeric(1),
+	kitchOption Numeric(1),
+	coffeeOption Numeric(1),
+	accessibleOption Numeric(1),
+	roomBookedQuantity Numeric(3),
+	roomBooked CHAR(1) check (roomBooked in ('N', 'Y')),
+	roomCostPerNight Numeric(4),
+	roomStatus VARCHAR(20)	
+);
+
+CREATE TABLE EMPLOYEE (
+	employeeID Numeric(3) primary key,
+	employeeUsername VARCHAR(30),
+	employeeName VARCHAR(30),
+	employeePassword VARCHAR(30)
+);
+
+CREATE TABLE ROOMSERVICE (
+	roomServiceID Numeric(3) primary key,
+	description VARCHAR(30),
+	price Numeric(4)
+);
+
+CREATE TABLE VALUEGUEST (
+	valueClubID Numeric(3) primary key,
+	numberOfBookings Numeric(3),
+	amountSpentWithHotel Numeric(5)
+	guestID Numeric(3),
+	guestUsername VARCHAR(30),
+	guestName VARCHAR(30),
+	guestPassword VARCHAR(30)
+);
+
+CREATE TABLE GUEST (
+	guestID Numeric(3) primary key,
+	guestUsername VARCHAR(30),
+	guestName VARCHAR(30),
+	guestPassword VARCHAR(30)
+);
+
+CREATE TABLE BOOKING (
+	 bookedRoom ROOM
+	 bookingGuest GUEST 
+);
