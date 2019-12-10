3b256d72843771da23d5ed31a0555534759b8147
taylo8am <57958597+taylo8am@users.noreply.github.com>
2019-12-09 21:00:10 -0500

    Update HotelMadisonDDL.sql
    added dummy data to DDL

--- HotelMadisonDDL.sql
+++ HotelMadisonDDL.sql
@@ -47,6 +47,19 @@ CREATE TABLE ROOMSERVICE (
 	roomServiceID Numeric(3) primary key,
 	serviceBookingID Numeric(3)
 		references BOOKING(bookingID) not null,
-	description VARCHAR(30),
+	description VARCHAR(75),
 	price Numeric(4)
 );
+
+insert into employee values (0, 'Adam', 'Adam Hitchings', 'pass');
+insert into guest values (0, 'Brendan', 'Brendan Boyle', 'pass', null);
+insert into guest values (1, 'George', 'George Moya', 'pass', 0);
+insert into room values (0, 100, 0, 0, 0, 0, 0, 0, 200.0, 'Active');
+insert into room values (1, 201, 1, 1, 1, 0, 0, 0, 200.0, 'Active');
+insert into room values (2, 302, 2, 1, 0, 0, 0, 0, 300.0, 'Active');
+insert into room values (3, 308, 2, 0, 0, 0, 0, 0, 150.0, 'Active');
+insert into room values (4, 400, 1, 1, 1, 1, 0, 0, 250.0, 'Active');
+insert into booking values (0, 1, 0, 2019, 249, 255);
+insert into booking values (1, 2, 1, 2019, 108, 110);
+insert into roomservice values (0, 1, 'beer', 50.0);
+insert into roomservice values (1, 1, 'food', 70.0);
